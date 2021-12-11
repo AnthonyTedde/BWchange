23d5a5aafe90e706daa22473cabfcd936c2f36b7
AnthonyTedde <antho.tedde@gmail.com>
2021-12-11 17:56:27 +0100

    update and add models

--- 03.Calibration/tidy_models/specifications/models_spec.R
+++ 03.Calibration/tidy_models/specifications/models_spec.R
@@ -1,155 +1,175 @@
+rm(list = ls())
 library(magrittr)
 
 
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+#-------------------------------------------------------------------------------
 # Linear based models ####
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+#-------------------------------------------------------------------------------
+
 
 # Ordinary least square
-#-+-+-+-+-+-+-+-+-+-+-+-
+#---------------------
+
 lm_spec <- parsnip::linear_reg() %>%
   parsnip::set_engine("lm")
 
 # Which hyper parameters could be tuned ?
 tune::tunable(lm_spec) # No one... what a shame XD
 
+
 # Generalized Linear Model
-#-+-+-+-+-+-+-+-+-+-+-+-+-+-
+#---------------------
+
 glm_spec <- parsnip::linear_reg(penalty = tune::tune(),
                                 mixture = tune::tune()) %>%
-  parsnip::set_engine(engine = "glmnet", family = Gamma)
+  parsnip::set_engine(engine = "glmnet", family = gaussian(link = "log"))
 
 # Which hyper parameters could be tuned ?
-tune::tunable(glm_spec)
-# * penalty: Level of regularization
-# * mixture: Proportion of Lasso (mixture = 1) and ridge (mixture = 0)
+tune::tunable(glm_spec) # penalty and mixture.
 
-# Not hyper parameter, but should be set:
-# * What about family ? --> Gaussian as set by default.
 
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+# Partial least square
+#---------------------
+
+tune::tunable(plsmod::pls() %>% parsnip::set_engine("mixOmics"))
+
+pls_spec <- plsmod::pls(
+  num_comp = tune::tune(),
+  predictor_prop = tune::tune()
+) %>%
+  parsnip::set_engine("mixOmics") %>%
+  parsnip::set_mode("regression")
+
+#-------------------------------------------------------------------------------
 # Trees based methods ####
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+#-------------------------------------------------------------------------------
+
 
 # Random forest
-#-+-+-+-+-+-+-+-
+#---------------------
+
 rf_spec <- parsnip::rand_forest(
   mtry = tune::tune(),
-  trees = ntrees,
+  trees = tune::tune(),
   min_n = tune::tune()
 ) %>%
   parsnip::set_mode(mode = "regression") %>%
   parsnip::set_engine(engine = "ranger")
 
 # Which hyper parameters could be tuned ?
-tune::tunable(rf_spec)
-# * mtry: Number of selected predictors
-# * trees: number of trees (see file globals/globals-models.R)
-# * min_n: Minimal node side (below this number a node can't be split)
+tune::tunable(rf_spec) #mtry, trees, min_n
+
 
 # XgBoost
-#-+-+-+-+-
+#---------------------
+
 xgb_spec <- parsnip::boost_tree(
-  trees = ntrees,
-  tree_depth = tune::tune("xgb_tree_depth"),
-  min_n = tune::tune("xgb_min_n"),
+  tree_depth = tune::tune(),
+  trees = tune::tune(),
+  learn_rate = tune::tune(),
+  mtry = tune::tune(),
+  min_n = tune::tune(),
   loss_reduction = tune::tune(),
   sample_size = tune::tune(),
-  mtry = tune::tune(),
-  learn_rate = tune::tune(),
+  stop_iter = tune::tune()
 ) %>%
   parsnip::set_engine("xgboost") %>%
   parsnip::set_mode(mode = "regression")
 
 # Which hyper parameters could be tuned ?
 tune::tunable(xgb_spec)
-# * trees: number of trees (see file globals/globals-models.R)
-# * tree_depth: Maximum depth of the tree
-# * min_n: Minimal node side (below this number a node can't be split)
-# * loss_reduction: Minimal reduction achieved in the loss function to split further
-# * sample_size: Proportion of data exposed to the fitting routine
-# * mtry: Number of selected predictors
-# TODO * learn_rate: euuuh.....
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# * stop_iter: The number of iteration without improvement before stropping
 
 
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+#-------------------------------------------------------------------------------
 # Distance based methods ####
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+#-------------------------------------------------------------------------------
+
 
 # K nearest neighbor
-#-+-+-+-+-+-+-+-+-+-+-
+#---------------------
+
 knn_spec <- parsnip::nearest_neighbor(
   neighbors = tune::tune(),
-  weight_func = tune::tune()
+  weight_func = tune::tune(),
+  dist_power = tune::tune()
 ) %>%
   parsnip::set_mode(mode = "regression") %>%
   parsnip::set_engine(engine = "kknn")
 
 # Which hyper parameters could be tuned ?
 tune::tunable(knn_spec)
-# * neighbors: k (number of neighbors to consider)
-# * weight_func: Kernel function
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# * dist_power: related to the minkowski distance
 
 
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+#-------------------------------------------------------------------------------
 # Support vector machine ####
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+#-------------------------------------------------------------------------------
+
 
 # Linear SVM
-#-+-+-+-+-+-+-+-+-+-+-
-linearSVM_spec <- parsnip::svm_linear(
+#---------------------
+
+# Which hyper parameters could be tuned ?
+tune::tunable(parsnip::svm_linear())
+
+svm_linear_spec <- parsnip::svm_linear(
   cost = tune::tune(),
   margin = tune::tune()
 ) %>%
   parsnip::set_mode("regression") %>%
   parsnip::set_engine("LiblineaR")
 
-# Which hyper parameters could be tuned ?
-tune::tunable(linearSVM_spec)
-# * cost: Cost of predicting a sample outside (kind of regularization parameter)
-# * margin: epsilon
 
 # Polynomial SVM
-#-+-+-+-+-+-+-+-+-+-+-
-polySVM_spec <- parsnip::svm_poly(
+#---------------------
+
+# Which hyper parameters could be tuned ?
+tune::tunable(parsnip::svm_poly())
+
+svm_poly_spec <- parsnip::svm_poly(
   cost = tune::tune(),
-  margin = tune::tune(),
   degree = tune::tune(),
-  scale_factor = tune::tune()
+  scale_factor = tune::tune(),
+  margin = tune::tune()
 ) %>%
   parsnip::set_mode("regression") %>%
   parsnip::set_engine("kernlab")
 
-# Which hyper parameters could be tuned ?
-tune::tunable(polySVM_spec)
-# * cost: Cost of predicting a sample outside (kind of regularization parameter)
-# * margin: epsilon
-# * degree: Polynomial degree
-# * scale_factor: Polynomial scaling factor
 
 # Radial basis SVM
-#-+-+-+-+-+-+-+-+-+-+-
-rbfSVM_spec <- parsnip::svm_rbf(
+#---------------------
+
+# Which hyper parameters could be tuned ?
+tune::tunable(parsnip::svm_rbf())
+
+svm_rbf_spec <- parsnip::svm_rbf(
   cost = tune::tune(),
   margin = tune::tune(),
   rbf_sigma = tune::tune()
 ) %>%
   parsnip::set_mode("regression") %>%
   parsnip::set_engine("kernlab")
-# Which hyper parameters could be tuned ?
-tune::tunable(rbfSVM_spec)
-# * cost: Cost of predicting a sample outside (kind of regularization parameter)
-# * margin: epsilon
-# * rbf_sigma: # for the radial basis function
 
 
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+#-------------------------------------------------------------------------------
+# Artificial Neural Net ####
+#-------------------------------------------------------------------------------
+
+tune::tunable(parsnip::mlp(engine = "keras"))
+
+mlp_spec <- parsnip::mlp(
+  hidden_units = tune::tune(),
+  penalty = tune::tune(),
+  epochs = tune::tune(),
+  activation = tune::tune()
+) %>%
+  parsnip::set_mode("regression") %>%
+  parsnip::set_engine("keras")
+
+#-------------------------------------------------------------------------------
 # Save ####
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+#-------------------------------------------------------------------------------
+
+grep(pattern = "\\_spec", ls(), value = T)
 
 save(lm_spec,
      file = here::here("data", "lm_spec.rda"),
