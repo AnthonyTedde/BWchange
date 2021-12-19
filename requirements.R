6cf73b390790ec45b9945cf4251dbc76bf125bdb
AnthonyTedde <antho.tedde@gmail.com>
2021-12-19 18:40:10 +0100

    update

--- requirements.R
+++ requirements.R
@@ -1,7 +1,7 @@
 packages_lst <- c("glue", "pls", "tibble", "stringr", "rsample", "parsnip",
                   "tune", "tidy", "here", "dials", "recipes", "purrr",
                   "workflowsets", "doParallel", "BiocManager", "magrittr",
-                  "tidymodels", "tidyverse", "kernlab")
+                  "tidymodels", "tidyverse", "kernlab", "keras", "plsmod")
 
 
 install.packages(pkgs = packages_lst,
